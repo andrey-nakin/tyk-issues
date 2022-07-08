939bd42318aee3e75ab696bebc7e4192697931b5
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2022-07-08 18:46:06 +0300

    Initial commit

---
+++ src/test/java/com/example/gqlapi/GqlapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GqlapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
