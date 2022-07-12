e8a6e27d4b523f93f4d47dbf0fb4d87493c70f0e
Andrey Nakin <A.Nakin@pulkovo-airport.com>
2022-07-12 17:57:33 +0300

    Intermediate commit

--- src/main/java/com/example/gqlapi/model/MyMutationPayload.java
+++
@@ -1,3 +0,0 @@
-package com.example.gqlapi.model;
-
-public interface MyMutationPayload {}
--- src/main/java/com/example/gqlapi/model/NullArgumentError.java
+++
@@ -1,8 +0,0 @@
-package com.example.gqlapi.model;
-
-import java.util.Collections;
-import java.util.List;
-
-public class NullArgumentError extends AbstractUserError implements MyMutationError {
-
-}
